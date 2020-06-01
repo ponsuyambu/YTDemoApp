66b34e4600096d3fd93eec43a4b3511e0f4850a3
Pons <suyambu.vps@gmail.com>
2020-06-01 19:16:50 +0200

    Initial import

---
+++ app/src/main/java/in/ponshere/ytdemoapp/AuthenticationScreen.kt
@@ -0,0 +1,11 @@
+package `in`.ponshere.ytdemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticationScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
