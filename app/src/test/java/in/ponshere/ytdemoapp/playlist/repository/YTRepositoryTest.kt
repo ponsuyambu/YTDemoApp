13d165184e94b506a24f7bf94d4998da33073aff
Pons <suyambu.vps@gmail.com>
2020-06-03 18:45:18 +0200

    Handle `should not add playlist result to local data source if it is already cached`

--- app/src/test/java/in/ponshere/ytdemoapp/playlist/repository/YTRepositoryTest.kt
+++ app/src/test/java/in/ponshere/ytdemoapp/playlist/repository/YTRepositoryTest.kt
@@ -4,6 +4,7 @@ import `in`.ponshere.ytdemoapp.datasource.YTLocalDataSource
 import `in`.ponshere.ytdemoapp.datasource.YTRemoteDataSource
 import `in`.ponshere.ytdemoapp.playlist.viewmodels.mockPlaylistResultWithToken
 import `in`.ponshere.ytdemoapp.utils.TestCoroutineRule
+import `in`.ponshere.ytdemoapp.utils.any
 import `in`.ponshere.ytdemoapp.utils.eq
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -14,7 +15,7 @@ import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
 private const val A_PAGE_TOKEN: String = "a_page_token"
@@ -43,12 +44,23 @@ class YTRepositoryTest {
     }
 
     @Test
-    fun `should add playlist result if it is not already cached` () = testCoroutineRule.runBlocking {
-        Mockito.`when`(remoteDataSource.getPlaylists(Mockito.anyString())).thenReturn(mockPlaylistResultWithToken)
-        Mockito.`when`(localDataSource.isAlreadyCached(Mockito.anyString())).thenReturn(false)
+    fun `should add playlist result to local data source if it is not already cached` () = testCoroutineRule.runBlocking {
+        `when`(remoteDataSource.getPlaylists(anyString())).thenReturn(mockPlaylistResultWithToken)
+        `when`(localDataSource.isAlreadyCached(anyString())).thenReturn(false)
 
         repository.getPlaylists(A_PAGE_TOKEN)
 
-        Mockito.verify(localDataSource).addPlaylistResult(eq(mockPlaylistResultWithToken))
+        verify(localDataSource).addPlaylistResult(eq(mockPlaylistResultWithToken))
     }
+
+    @Test
+    fun `should not add playlist result to local data source if it is already cached` () = testCoroutineRule.runBlocking {
+        `when`(remoteDataSource.getPlaylists(anyString())).thenReturn(mockPlaylistResultWithToken)
+        `when`(localDataSource.isAlreadyCached(anyString())).thenReturn(true)
+
+        repository.getPlaylists(A_PAGE_TOKEN)
+
+        verify(localDataSource,never()).addPlaylistResult(any())
+    }
+
 }
